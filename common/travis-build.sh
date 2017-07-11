50645f176c50c5ee24f281968b63bc0f21291a79
tamutamu <tamu.0.0.tamu@gmail.com>
2017-07-11 11:03:01 +0900

    Update.

--- common/travis-build.sh
+++ common/travis-build.sh
@@ -18,23 +18,16 @@ popd
 # if it merges pull request to develop branch or dilectly commit on develop branch, execute `gradlew uploadArchives`.
 # Waning, TRAVIS_PULL_REQUEST variable is 'false' or pull request number, 1,2,3 and so on.
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "develop"  ]; then
-  ./gradlew uploadArchives -PnablarchRepoUsername=${REPO_USER} -PnablarchRepoPassword=${DEPLOY_PASSWORD} \
+  ./gradlew clean test uploadArchives -PnablarchRepoUsername=${REPO_USER} -PnablarchRepoPassword=${DEPLOY_PASSWORD} \
                            -PnablarchRepoReferenceUrl=${DEVELOP_REPO_URL} -PnablarchRepoReferenceName=${DEVELOP_REPO_NAME} \
                            -PnablarchRepoDeployUrl=dav:${DEVELOP_REPO_URL} -PnablarchRepoName=${DEVELOP_REPO_NAME} \
                            --no-daemon
 else
-  ./gradlew build -PnablarchRepoUsername=${REPO_USER} -PnablarchRepoPassword=${DEPLOY_PASSWORD} \
+  ./gradlew clean test -PnablarchRepoUsername=${REPO_USER} -PnablarchRepoPassword=${DEPLOY_PASSWORD} \
                   -PnablarchRepoReferenceUrl=${DEVELOP_REPO_URL} -PnablarchRepoReferenceName=${DEVELOP_REPO_NAME} \
                   -PnablarchRepoDeployUrl=dav:${DEVELOP_REPO_URL} -PnablarchRepoName=${DEVELOP_REPO_NAME} \
                   --no-daemon
 fi
-
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-ls ${TRAVIS_BUILD_DIR}
-ls ${TRAVIS_BUILD_DIR}/build/
-ls ${TRAVIS_BUILD_DIR}/build/reports/
-
-exit
 
 
 # Upload Unit test report.
